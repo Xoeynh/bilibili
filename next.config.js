39fcc18f3df7f38779c1d96c62546bf140a58336
xlz122 <122874722@qq.com>
2022-10-23 21:50:41 +0800

    chore: 配置安全标头(xxs保护)

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+const securityHeaders = [
+  // xxs保护
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block'
+  }
+];
+
 const nextConfig = {
   basePath: process.env.NEXT_PUBLIC_BASE_URL,
   reactStrictMode: true,
@@ -19,6 +28,15 @@ const nextConfig = {
   // 删除所有console.*
   compiler: {
     removeConsole: process.env.NODE_ENV === 'production' ? true : false
+  },
+  // 安全标头
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ]
   }
 };
 
