5bb2030a2683212f7add0d0420a15d006a4d5325
xlz122 <122874722@qq.com>
2022-10-24 11:13:34 +0800

    feta: html根标签的font-size大小适配

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
+import { useEffect } from 'react';
 import store from '@/store/index';
 import { Provider } from 'react-redux';
+import adapter from '@/utils/adapter';
 import type { ReactElement } from 'react';
 import type { AppProps } from 'next/app';
 import Layout from '@components/layout/Layout';
@@ -7,6 +9,16 @@ import '@/styles/globals.css';
 import '@/styles/not-found.scss';
 
 function MyApp({ Component, pageProps }: AppProps): ReactElement {
+  // 字体适配
+  useEffect(() => {
+    adapter();
+    window.addEventListener('resize', adapter);
+
+    return () => {
+      window.removeEventListener('resize', adapter);
+    };
+  }, []);
+
   return (
     <Provider store={store}>
       <Layout>
