8274478841ed32089274c9eb0b3f11748ffd6994
xlz122 <122874722@qq.com>
2022-12-09 15:30:34 +0800

    feat: 搜索回车未输入时使用默认关键词

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -64,22 +64,24 @@ function Search(props: Props): React.ReactElement {
   };
 
   // 搜索回车
-  const handleEnterKey = (e: InputEnter): boolean | undefined => {
-    if (!e.target.value) {
-      return false;
-    }
-
+  const handleEnterKey = (e: InputEnter): void => {
     if (e.nativeEvent.code === 'Enter') {
+      const defaultValue = props?.search?.default?.show_name;
+
+      setSearchValue(e.target.value || defaultValue || '');
+
       router.push({
         pathname: '/search',
         query: {
-          keyword: e.target.value
+          keyword: e.target.value || defaultValue
         }
       });
 
       store.dispatch({
         type: 'routine/setSearchHistory',
-        payload: Array.from(new Set([...searchHistory, e.target.value]))
+        payload: Array.from(
+          new Set([...searchHistory, e.target.value || defaultValue])
+        )
       });
     }
   };
